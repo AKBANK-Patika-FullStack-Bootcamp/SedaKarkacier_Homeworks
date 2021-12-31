fe7883142716a2a9fdfea4cefe9003c84ac69cf6
Seda Karkacıer <95433892+Skarkacier@users.noreply.github.com>
2021-12-31 14:09:41 +0300

    Delete Week1-2/Homework1/PharmacyStock/PharmacyStock directory

--- Week1-2/Homework1/PharmacyStock/PharmacyStock/Model/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PharmacyStock.Model
-{
-    public class Result
-    {
-        public int status { get; set; }
-        public string? message { get; set; }
-        public List<Medicine>? MedicinesList { get; set; }
-    }
-}
