8e40fc0cf3ce84014a548978cbba812455f6c96d
Seda Karkacıer <95433892+Skarkacier@users.noreply.github.com>
2022-01-14 16:46:08 +0300

    Add files via upload

---
+++ Homework4/DAL/Model/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Model
+{
+    public class Result
+    {
+        public int status { get; set; }
+        public string? message { get; set; }
+        public List<Medicine>? MedicinesList { get; set; }
+    }
+}
---
+++ Homework4/Entities/MedicineContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Entities
+{
+    public class MedicineContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public MedicineContext() 
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //options.UseSqlServer(Configuration.GetConnectionString("MedicineDBEntities"));
+            options.UseSqlServer("Data Source = DESKTOP-G69HVIR; Database = Pharmacy_Stock; integrated security = true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Medicine>().ToTable("Medicine");
+            modelBuilder.Entity<Features>().ToTable("Features");
+        }
+        public DbSet<Medicine> Medicine { get; set; }
+        public DbSet<Features> Features { get; set; }
+    }
+}
